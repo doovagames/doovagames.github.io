c33cf2d1684006304e2a2a841bc63c15736ea65d
Hassan <hbahemia@elam.co.uk>
2019-03-08 10:18:48 +0000

    fixing slides

--- scripts.js
+++ scripts.js
@@ -24,7 +24,8 @@ function topFunction() {
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
+/*
 setInterval(carousel("visual", "images/visual0.png", "images/visual1.png", "images/visual2.png", "images/visual3.png", "images/visual4.png", "images/visual5.png", "images/visual6.png"), 3000);
 carousel("mechanics", "images/mechanics0.png", "images/mechanics1.png", "images/mechanics2.png", "images/mechanics3.png");
 carousel("sound", "images/sound0.png", "images/sound1.png", "images/sound2.png", "images/sound3.png");
-
+*/
